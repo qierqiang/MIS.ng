c3610b200fd83f64b817b06dc3b07773fa6bb8ba
qierqiang <erqiang.qi@gmail.com>
2016-06-04 12:57:08 +0800

    临时提交0604

---
+++ MainFrame.js
@@ -0,0 +1,9 @@
+(function () {
+    var app = angular.module("app", []);
+
+    app.directive("horizontal-scroll", function () {
+        return {
+             template:``,
+        };
+    });
+})();
